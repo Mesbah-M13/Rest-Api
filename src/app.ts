6c8c946dbd9fce596010dc8fe50e7d85bc399f47
Mesbah-M13 <mesbah0010@gmail.com>
2025-10-31 00:51:54 +0600

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,19 @@
-import express, { type NextFunction, type Request, type Response } from 'express';
-import type { HttpError } from 'http-errors';
-import { config } from './config/config.js';
+import express from 'express';
 import createHttpError from 'http-errors';
+import globalErrorHandler from './middlewares/globalErrorHandler.js';
+import userRouter from './user/userRouter.js';
 
 const app = express();
 
+app.use('/api/users', userRouter);
+
+// global error handlers
 app.get('/', (req, res, next) => {
 	const error = createHttpError(400, 'Something went wrong');
 	throw error;
 	res.json({ message: 'Welcome to e-libray APIS' });
 });
 
-// global error handlers -- it has 4 parameters
-// use() --> use to register middlewares
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-	const statusCode = err.statusCode || 500;
+app.use(globalErrorHandler);
 
-	return res.status(statusCode).json({
-		message: err.message,
-		errorStack: config.env === 'development' ? err.stack : '',
-	});
-});
 export default app;
---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,16 @@
+import express, { type NextFunction, type Request, type Response } from 'express';
+import type { HttpError } from 'http-errors';
+import { config } from '../config/config.js';
+
+// global error handlers -- it has 4 parameters
+// use() --> use to register middlewares
+const globalErrorHandler = (err: HttpError, req: Request, res: Response, next: NextFunction) => {
+	const statusCode = err.statusCode || 500;
+
+	return res.status(statusCode).json({
+		message: err.message,
+		errorStack: config.env === 'development' ? err.stack : '',
+	});
+};
+
+export default globalErrorHandler;
