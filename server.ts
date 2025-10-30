513e33a54f00a2c5723467cb68fdbf3769d1f7bc
Mesbah-M13 <mesbah0010@gmail.com>
2025-10-30 15:25:25 +0600

    add mongo db connection

--- server.ts
+++ server.ts
@@ -1,12 +1,16 @@
 import app from "./src/app.js";
 import { config } from "./src/config/config.js";
+import connectDB from "./src/config/db.js";
 
-const startServer = () => {
-  const port = config.port || 4000;
+const startServer = async () => {
+	// connect db
+	await connectDB();
 
-  app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
-  });
+	const port = config.port || 4000;
+
+	app.listen(port, () => {
+		console.log(`Listening on port: ${port}`);
+	});
 };
 
 startServer();
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res, next) => {
-  res.json({ message: "Welcome to e-libray APIS" });
+	res.json({ message: "Welcome to e-libray APIS" });
 });
 
 export default app;
