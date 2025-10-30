83848e92016a443134d8931130ab962e4d425b3c
Mesbah-M13 <mesbah0010@gmail.com>
2025-10-30 19:20:54 +0600

    add global error handlers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
-import express from "express";
+import express, { type NextFunction, type Request, type Response } from 'express';
+import type { HttpError } from 'http-errors';
+import { config } from './config/config.js';
+import createHttpError from 'http-errors';
 
 const app = express();
 
-app.get("/", (req, res, next) => {
-	res.json({ message: "Welcome to e-libray APIS" });
+app.get('/', (req, res, next) => {
+	const error = createHttpError(400, 'Something went wrong');
+	throw error;
+	res.json({ message: 'Welcome to e-libray APIS' });
 });
 
+// global error handlers -- it has 4 parameters
+// use() --> use to register middlewares
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+	const statusCode = err.statusCode || 500;
+
+	return res.status(statusCode).json({
+		message: err.message,
+		errorStack: config.env === 'development' ? err.stack : '',
+	});
+});
 export default app;
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,19 +1,20 @@
-import mongoose from "mongoose";
-import { config } from "./config.js";
+import mongoose from 'mongoose';
+import { config } from './config.js';
 
 const connectDB = async () => {
 	try {
-		mongoose.connection.on("connected", () => {
-			console.log("Connected to database successfully");
+		// here register 1st
+		mongoose.connection.on('connected', () => {
+			console.log('Connected to database successfully');
 		});
 
-		mongoose.connection.on("error", (err) => {
-			console.log("Error in connecting to database.", err);
+		mongoose.connection.on('error', (err) => {
+			console.log('Error in connecting to database.', err);
 		});
 
 		await mongoose.connect(config.databaseUrl as string);
 	} catch (err) {
-		console.error("Failed to connect to database.", err);
+		console.error('Failed to connect to database.', err);
 		process.exit(1);
 	}
 };
