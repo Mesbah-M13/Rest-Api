b9694ad5fa4a3c6602e08cb3c0c8f5ae05b199a4
Mesbah-M13 <mesbah0010@gmail.com>
2025-11-01 08:53:55 +0600

    change create user status code to 201

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -59,4 +59,61 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 	}
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+	res.json({ message: 'Ok' });
+};
+
+// const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+// 	// Validation
+// 	const {  email, password } = req.body;
+
+// 	if ( !email || !password) {
+// 		const error = createHttpError(400, 'All fields are required');
+
+// 		return next(error);
+// 	}
+
+// 	// Database Call
+// 	try {
+// 		const user = await userModel.findOne({ email: email });
+
+// 		if (user) {
+// 			const error = createHttpError(400, 'User already exist in this email');
+// 			return next(error);
+// 		}
+// 	} catch (err) {
+// 		return next(createHttpError(500, 'Error while getting user'));
+// 	}
+
+// 	// Password - hashed
+// 	const hashedPassword = await bcrypt.hash(password, 10);
+// 	let newUser: User;
+// 	try {
+// 		newUser = await userModel.create({
+// 			name,
+// 			email,
+// 			password: hashedPassword,
+// 		});
+// 	} catch (err) {
+// 		return next(createHttpError(500, 'Error while creating user'));
+// 	}
+
+// 	try {
+// 		// Token generation
+// 		const token = jwt.sign({ sub: newUser._id }, config.jwtSecret as string, {
+// 			expiresIn: '7d',
+// 			algorithm: 'HS256',
+// 		});
+
+// 		// Process
+
+// 		// Response
+
+// 		// res.json({ id: newUser._id });
+// 		res.json({ accessToken: token });
+// 	} catch (err) {
+// 		return next(createHttpError(500, 'Error while signing the JWT token'));
+// 	}
+// };
+
+export { createUser, loginUser };
