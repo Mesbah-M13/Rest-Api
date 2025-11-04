4ea0b1a82054b5b87eeabccb91df300ada81b015
Mesbah-M13 <mesbah0010@gmail.com>
2025-11-04 11:02:55 +0600

    update book functionalities

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createBook } from './bookController.js';
+import { createBook, updateBook } from './bookController.js';
 import multer from 'multer';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -24,6 +24,15 @@ bookRouter.post(
 		{ name: 'file', maxCount: 1 },
 	]),
 	createBook
+);
+bookRouter.patch(
+	'/:bookId',
+	authenticate,
+	upload.fields([
+		{ name: 'coverImage', maxCount: 1 },
+		{ name: 'file', maxCount: 1 },
+	]),
+	updateBook
 );
 
 export default bookRouter;
