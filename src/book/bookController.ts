bcf8a674baad1cb8813ba856f96b613a0ec70024
Mesbah-M13 <mesbah0010@gmail.com>
2025-11-04 07:46:49 +0600

    create book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,14 +1,16 @@
 import type { NextFunction, Request, Response } from 'express';
+import fs from 'node:fs';
 import cloudinary from '../config/cloudinary.js';
-import * as path from 'path';
 import { fileURLToPath } from 'url';
+import path from 'path';
 import createHttpError from 'http-errors';
+import bookModel from './bookModel.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-	// const {} = req.body;
+	const { title, genre } = req.body;
 
 	console.log('files', req.files);
 
@@ -25,10 +27,10 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
 		format: coverImageMimeType,
 	});
 
-	const bookFileName = files.file[0].filename;
-	const bookFilePath = path.resolve(__dirname, '../../public/data/uploads', bookFileName);
-
 	try {
+		const bookFileName = files.file[0].filename;
+		const bookFilePath = path.resolve(__dirname, '../../public/data/uploads', bookFileName);
+
 		const bookFileUploadResult = await cloudinary.uploader.upload(bookFilePath, {
 			resource_type: 'raw',
 			filename_override: bookFileName,
@@ -36,14 +38,46 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
 			format: 'pdf',
 		});
 
-		console.log('bookFileUploadResult', bookFileUploadResult);
+		// console.log('bookFileUploadResult', bookFileUploadResult);
+		// console.log('UploadResult', uploadResult);
 
-		console.log('UploadResult', uploadResult);
+		console.log('Trying to delete:', filePath, bookFilePath);
 
-		res.json({});
+		const newBook = await bookModel.create({
+			title,
+			genre,
+			author: '690537df68c9d401eb53455b',
+			coverImage: uploadResult.secure_url,
+			file: bookFileUploadResult.secure_url,
+		});
+
+		// Delete temp.files
+
+		// try {
+		// 	await fs.promises.unlink(filePath);
+		// 	await fs.promises.unlink(bookFilePath);
+		// } catch (err) {
+		// 	console.warn('⚠️ Failed to delete one or more temporary files:', err);
+		// 	// Don’t throw here — since file deletion is cleanup, not a critical failure
+		// }
+
+		try {
+			console.log('Deleting temp files...');
+			console.log('filePath:', filePath);
+			console.log('bookFilePath:', bookFilePath);
+
+			await fs.promises.unlink(filePath);
+			await fs.promises.unlink(bookFilePath);
+
+			console.log('✅ Temp files deleted successfully');
+		} catch (err) {
+			console.error('❌ Failed to delete temp files:', err.message);
+		}
+
+		res.status(201).json({ id: newBook._id });
 	} catch (err) {
 		console.log(err);
-		return next(createHttpError(500, 'Error while uploading the files.'));
+		return next(createHttpError(500, '🪫Error while uploading the files.'));
 	}
 };
 
